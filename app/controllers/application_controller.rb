df3ad6d46570c54196d9a60032835a218fd64a65
tachikawa motoki <tachikawamotoki@tachikawamotoonoMacBook-Air.local>
2023-03-11 12:14:14 +0900

    セッション管理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :createlogin
+  def createlogin
+    if User.find_by(id:session[:user_id])
+      @current_user = User.find_by(id:session[:user_id])
+    else
+      @current_icon_id = rand(10)
+      @current_icon_color_id = rand(10)
+      @current_user = User.new(icon_id: @current_icon_id,icon_back_id: @current_icon_color_id)
+      @current_user.save
+      session[:user_id] = @current_user.id
+    end
+  end
 end
