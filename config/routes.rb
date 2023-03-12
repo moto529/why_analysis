94b1b85ea3e192da441bbcfbafda04663a2864b7
tachikawa motoki <tachikawamotoki@tachikawamotoonoMacBook-Air.local>
2023-03-13 01:25:34 +0900

    詳細機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :analyses, only: %i[new create] do
+  resources :analyses, only: %i[new create show] do
     resources :is_whies, only: %i[new create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
